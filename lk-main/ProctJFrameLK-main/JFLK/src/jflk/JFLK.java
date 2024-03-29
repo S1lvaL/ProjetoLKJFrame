a991b4303b9d7862724614f245d39aee661acc25
S1lvaL <152618404+S1lvaL@users.noreply.github.com>
2024-03-29 12:32:45 -0300

    Update JFLK.java

--- lk-main/ProctJFrameLK-main/JFLK/src/jflk/JFLK.java
+++ lk-main/ProctJFrameLK-main/JFLK/src/jflk/JFLK.java
@@ -494,7 +494,7 @@ public class JFLK extends javax.swing.JFrame {
             if (curso.isEmpty()) {
                 txtcur.setText("Informe o curso!");
             }
-            if ((!year.isEmpty()) && responsavel.isEmpty() && idade < 18 ) {
+            if (responsavel.isEmpty() && idade < 18 ) {
                 txtrep.setVisible(true);
                 txtrep.setText("Informe o responsavel!");
             } 
